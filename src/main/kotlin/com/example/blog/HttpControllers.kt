ecc45177164ec1e8597f131f7b68aa285898dbd2
easywaldo <easywaldoji@gmail.com>
2023-05-01 16:19:15 +0900

    lowercase blog slug - findOne API modified

--- src/main/kotlin/com/example/blog/HttpControllers.kt
+++ src/main/kotlin/com/example/blog/HttpControllers.kt
@@ -13,7 +13,7 @@ class ArticleController(private val repository: ArticleRepository) {
 
 	@GetMapping("/{slug}")
 	fun findOne(@PathVariable slug: String) =
-			repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
+			repository.findBySlug(slug.lowercase()) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
 
 }
 
